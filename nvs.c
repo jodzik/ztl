2bf778d455490b5dfd2aa140a82e222beb7e10db
jodzik <jodzikk@yandex.ru>
2026-04-19 15:30:05 +0700

    ench(nvs): add page size, count checks

--- nvs.c
+++ nvs.c
@@ -5,15 +5,19 @@
 LOG_MODULE_REGISTER(ztl_nvs);
 
 int ztl_nvs__init_manual(struct nvs_fs* fs, struct device const* flash_device, off_t offset, uint32_t size) {
-    struct flash_pages_info info = {0};
+    struct flash_pages_info page_info = {0};
 
     ASSERT(device_is_ready(flash_device), ER_NO_DEV);
 
     fs->flash_device = flash_device;
     fs->offset = offset;
-    TRY(flash_get_page_info_by_offs(flash_device, offset, &info));
-    fs->sector_size = info.size;
+
+    TRY(flash_get_page_info_by_offs(flash_device, 0, &page_info));
+    ASSERT(0 != page_info.size, ER_BAD_FILE);
+
+    fs->sector_size = page_info.size;
     fs->sector_count = size / fs->sector_size;
+    ASSERT(fs->sector_count >= 2, ER_BAD_FILE);
 
     TRY(nvs_mount(fs));
 
