50b92d79ba18c7cab1029c300a3c8f822e6fa697
jodzik <jodzikk@yandex.ru>
2026-05-20 00:36:51 +0700

    feat(nvs): add ring nvs

--- nvs.c
+++ nvs.c
@@ -4,6 +4,16 @@
 
 LOG_MODULE_REGISTER(ztl_nvs);
 
+enum {
+    MAX_NVS_RING_ID = ZTL_NVS_RING_MAX_EXTENT,
+    NVS_RING_SERVICE_ID = UINT16_MAX,
+};
+
+typedef struct __packed NvsRingServiceRecord {
+    uint16_t current_extent;
+    uint16_t last_nvs_id;
+} NvsRingServiceRecord;
+
 int ztl_nvs__init_manual(struct nvs_fs* fs, struct device const* flash_device, off_t offset, uint32_t size) {
     struct flash_pages_info page_info = {0};
 
@@ -23,6 +33,99 @@ int ztl_nvs__init_manual(struct nvs_fs* fs, struct device const* flash_device, o
 
     LOG_INF("FS was mounted: offset=%li size=%u sector_size=%u sector_count=%u",
         offset, size, fs->sector_size, fs->sector_count);
+
+    return 0;
+}
+
+int ztl_nvs_ring__init_manual(
+    struct ZtlNvsRing* const nvs_ring,
+    uint16_t const extent,
+    struct device const* const flash_device,
+    off_t const offset,
+    uint32_t const size)
+{
+    struct NvsRingServiceRecord service_rec = {0};
+    memset(nvs_read, 0, sizeof(*nvs_ring));
+    TRY(ztl_nvs__init_manual(&nvs_ring->fs, flash_device, offset, size));
+
+    nvs_ring->extent = extent;
+    ssize_t const check = nvs_read(&nvs_ring->fs, NVS_RING_SERVICE_ID, &service_rec, sizeof(service_rec));
+    if (sizeof(service_rec) == check) {
+        if (service_rec.current_extent > extent) {
+            nvs_ring->current_extent = extent;
+        } else {
+            nvs_ring->current_extent = service_rec.current_extent;
+        }
+        nvs_ring->last_nvs_id = service_rec.last_nvs_id;
+    }
+
+    return 0;
+}
+
+inline static uint16_t nvs_id_by_index(struct ZtlNvsRing const* const nvs_ring, uint16_t const index) {
+    if (index <= nvs_ring->last_nvs_id) {
+        return nvs_ring->last_nvs_id - index;
+    } else {
+        return MAX_NVS_RING_ID - index + nvs_ring->last_nvs_id + 1;
+    }
+}
+
+int ztl_nvs_ring__at(
+    struct ZtlNvsRing* const nvs_ring,
+    uint16_t const index,
+    uint8_t* const buf,
+    uint16_t const buf_size,
+    uint16_t* const real_size)
+{
+    ASSERT(NULL != nvs_ring, ER_INVAL);
+    ASSERT(NULL != buf, ER_INVAL);
+    ASSERT(buf_size > 0, ER_INVAL);
+    ASSERT(NULL != real_size, ER_INVAL);
+    ASSERT(index < nvs_ring->current_extent, ER_NO_ENT);
+
+    uint16_t const id = nvs_id_by_index(nvs_ring, index);
+    ssize_t const check = nvs_read(&nvs_ring->fs, id, buf, buf_size);
+    ASSERT(check > 0, (int)check);
+    ASSERT(check < UINT16_MAX, ER_OVERFLOW);
+    *real_size = (uint16_t)check;
+
+    return 0;
+}
+
+int ztl_nvs_ring__append(struct ZtlNvsRing* const nvs_ring, uint8_t const* const data, uint16_t const data_size) {
+    ASSERT(NULL != nvs_ring, ER_INVAL);
+    ASSERT(NULL != data, ER_INVAL);
+
+    if (nvs_ring->current_extent == nvs_ring->extent) {
+        uint16_t const id_to_delete = nvs_id_by_index(nvs_ring, nvs_ring->current_extent - 1);
+        TRY_PASS(nvs_delete(&nvs_ring->fs, id_to_delete));
+    }
+
+    if (MAX_NVS_RING_ID == nvs_ring->last_nvs_id) {
+        nvs_ring->last_nvs_id = 0;
+    } else {
+        nvs_ring->last_nvs_id += 1;
+    }
+
+    TRY(nvs_write(&nvs_ring->fs, nvs_ring->last_nvs_id, data, data_size));
+
+    return 0;
+}
+
+int ztl_nvs_ring__extent(struct ZtlNvsRing const* const nvs_ring, uint16_t* const extent) {
+    ASSERT(NULL != nvs_ring, ER_INVAL);
+    ASSERT(NULL != extent, ER_INVAL);
+
+    *extent = nvs_ring->extent;
+
+    return 0;
+}
+
+int ztl_nvs_ring__current_extent(struct ZtlNvsRing const* const nvs_ring, uint16_t* const current_extent) {
+    ASSERT(NULL != nvs_ring, ER_INVAL);
+    ASSERT(NULL != current_extent, ER_INVAL);
+
+    *current_extent = nvs_ring->current_extent;
 
     return 0;
 }
--- nvs.h
+++ nvs.h
@@ -5,9 +5,50 @@
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/fs/nvs.h>
 
+enum {
+    ZTL_NVS_RING_MAX_EXTENT = UINT16_MAX - 1,
+};
+
+typedef struct ZtlNvsRing {
+    struct nvs_fs fs;
+    uint16_t extent;
+    uint16_t current_extent;
+    uint16_t last_nvs_id;
+} ZtlNvsRing;
+
 int ztl_nvs__init_manual(struct nvs_fs* fs, struct device const* flash_device, off_t offset, uint32_t size);
 
 #define ztl_nvs__init(fs, partition) ztl_nvs__init_manual(fs, \
     FIXED_PARTITION_DEVICE(partition), FIXED_PARTITION_OFFSET(partition), FIXED_PARTITION_SIZE(partition))
+
+int ztl_nvs_ring__init_manual(
+    struct ZtlNvsRing* nvs_ring,
+    uint16_t extent,
+    struct device const* flash_device,
+    off_t offset,
+    uint32_t size);
+
+#define ztl_nvs_ring__init(nvs_ring, extent, partition) ztl_nvs_ring__init_manual(nvs_ring, extent, \
+    FIXED_PARTITION_DEVICE(partition), FIXED_PARTITION_OFFSET(partition), FIXED_PARTITION_SIZE(partition))
+
+/// @brief Get element from ring nvs buffer.
+/// @param nvs_ring[in] - nvs ring buffer.
+/// @param index[in] - index of element from end, 0 - last, newest element.
+/// @param buf[out] - element buffer.
+/// @param buf_size[in] - max size of element buf.
+/// @param real_size[out] - real size of read element.
+/// @return 0 is success, else if error.
+int ztl_nvs_ring__at(
+    struct ZtlNvsRing* nvs_ring,
+    uint16_t index,
+    uint8_t* buf,
+    uint16_t buf_size,
+    uint16_t* real_size);
+
+int ztl_nvs_ring__append(struct ZtlNvsRing* nvs_ring, uint8_t const* data, uint16_t data_size);
+
+int ztl_nvs_ring__extent(struct ZtlNvsRing const* nvs_ring, uint16_t* extent);
+
+int ztl_nvs_ring__current_extent(struct ZtlNvsRing const* nvs_ring, uint16_t* current_extent);
 
 #endif // ZTL_NVS_H_
