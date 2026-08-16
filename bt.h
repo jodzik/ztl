f9f7985b1f8adbf8beb7de4cf7a02e7e45dc67d1
jodzik <jodzikk@yandex.ru>
2026-08-16 13:10:52 +0700

    feat: add bt macro for create UUID by base UUID

--- bt.h
+++ bt.h
@@ -2,9 +2,22 @@
 #define ZTL_BT_H_
 
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/byteorder.h>
 #include <zephyr/bluetooth/uuid.h>
 #include <zephyr/bluetooth/gatt.h>
 #include <zephyr/zbus/zbus.h>
+
+#define ZTL_BT_UUID_128_ENCODE(manufacture_part, device_part, service_part, charc_part) \
+	BT_BYTES_LIST_LE64((uint64_t)(manufacture_part)),\
+	BT_BYTES_LIST_LE48((uint64_t)(device_part)), \
+	service_part, \
+	charc_part
+
+#define ZTL_BT_UUID_SERVICE(service_part_w8) BT_UUID_INIT_128(ZTL_BT_UUID_128_ENCODE( \
+    CONFIG_ZTL_BT_UUID_MANUFACTURE_PART, CONFIG_ZTL_BT_UUID_DEVICE_PART, service_part_w8, 0))
+
+#define ZTL_BT_UUID_CHARC(service_part_w8, charc_part_w8) BT_UUID_INIT_128(ZTL_BT_UUID_128_ENCODE( \
+    CONFIG_ZTL_BT_UUID_MANUFACTURE_PART, CONFIG_ZTL_BT_UUID_DEVICE_PART, service_part_w8, charc_part_w8))
 
 enum ZtlBtEventType {
     ZTL_BT_EVENT__CONNECTED,
